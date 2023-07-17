5c5fd5297844db2dd4764010eee9b63ba099c52c
YouTubeKosen <private.kosen@gmail.com>
2023-07-17 16:30:29 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t get_val(uint32_t * ptr)
+{
+    return *ptr;
+}
+
+int main(void) {
+    uint32_t hoge;
+    uint32_t *ptr_hoge;
+    hoge = 32;
+    printf("hoge = %d\n", hoge);
+
+    ptr_hoge = &hoge;
+    printf("hoge = %d\n", get_val(ptr_hoge));
+
+    return 0;
+}
