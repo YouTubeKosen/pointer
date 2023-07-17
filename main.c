893e4d90499cde9fdeac3efe23af28c64000a123
YouTubeKosen <private.kosen@gmail.com>
2023-07-17 21:33:25 +0900

    256 => 1

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main(void) {
     ptr_hoge_8bit = (uint8_t *)&hoge;
     printf("hoge = %d\n", get_val_8bit(ptr_hoge_8bit));
 
-    ptr_hoge_8bit += 1;
+    hoge += 1;
     printf("hoge = %d\n", get_val_8bit(ptr_hoge_8bit));
 
     return 0;
