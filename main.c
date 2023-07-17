c71c347360f5ed896b6e685c3d1d52179b500906
YouTubeKosen <private.kosen@gmail.com>
2023-07-17 16:38:39 +0900

    delete main

--- main.c
+++ main.c
@@ -6,14 +6,26 @@ uint32_t get_val(uint32_t * ptr)
     return *ptr;
 }
 
+uint32_t get_val_8bit(uint8_t * ptr)
+{
+    return *ptr;
+}
+
 int main(void) {
     uint32_t hoge;
     uint32_t *ptr_hoge;
+    uint8_t  *ptr_hoge_8bit;
     hoge = 256;
     printf("hoge = %d\n", hoge);
 
     ptr_hoge = &hoge;
     printf("hoge = %d\n", get_val(ptr_hoge));
+
+    ptr_hoge_8bit = (uint8_t *)ptr_hoge;
+    printf("hoge = %d\n", get_val_8bit(ptr_hoge_8bit));
+
+    ptr_hoge_8bit += 1;
+    printf("hoge = %d\n", get_val_8bit(ptr_hoge_8bit));
 
     return 0;
 }
