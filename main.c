c03df3c68d209028f8dd3a2eb7ad96966b5a1dad
YouTubeKosen <private.kosen@gmail.com>
2023-07-17 20:31:36 +0900

    rm get_val_32bit

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t get_val_32bit(uint32_t * ptr)
-{
-    return *ptr;
-}
-
 uint32_t get_val_8bit(uint8_t * ptr)
 {
     return *ptr;
@@ -13,14 +8,10 @@ uint32_t get_val_8bit(uint8_t * ptr)
 
 int main(void) {
     uint32_t hoge;
-    uint32_t *ptr_hoge;
     uint8_t  *ptr_hoge_8bit;
 
     hoge = 256;
     printf("hoge = %d\n", hoge);
-
-    ptr_hoge = &hoge;
-    printf("hoge = %d\n", get_val_32bit(ptr_hoge));
 
     ptr_hoge_8bit = (uint8_t *)&hoge;
     printf("hoge = %d\n", get_val_8bit(ptr_hoge_8bit));
